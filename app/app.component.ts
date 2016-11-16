ec7d61c3ada788437411a9e067ec52030d87a31c
PrasanthTR <mailprasanthtr@gmail.com>
2016-11-16 07:25:57 +0530

    Add .gitignore, basic wireframe

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,15 @@
 import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
-  template: '<h1>Hello Angular!</h1>'
+  templateUrl: './app/app.component.html'
 })
-export class AppComponent { }
+export class AppComponent { 
+  arrStr: String = "1,2,3,4,5";
+  ascArr = this.arrStr.split(",").sort();
+  descArr = this.arrStr.split(",").sort().reverse();
+   
+  onEnter () {
+    this.ascArr = this.arrStr.split(",").sort();
+    this.descArr = this.arrStr.split(",").sort().reverse();
+  }
+}
